f2784d97451333b0bcc05eb040eab9c6515331e1
paul karikari <paulkarikari1@gmail.com>
2018-01-19 22:19:34 +0000

    add services resource controller

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/logout', 'Auth\LoginController@logout');
+
+Route::middleware(['auth'])->group(function () {
+    Route::resource('services', 'ServicesController');
+});
