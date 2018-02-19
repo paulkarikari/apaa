8c8e9c5e386e7c678a59c4314f0544a6eb5a1ccb
paul karikari <paulkarikari1@gmail.com>
2018-02-19 15:38:00 +0000

    make user campany field nulluble

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -48,7 +48,7 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => 'required|string|max:255',
-            'company' => 'string|max:255',
+            'company' => 'nullable|string|max:255',
             'phone' => 'required|string|min:10|max:15',
             'user_type' => 'required|numeric',
             'email' => 'required|string|email|max:255|unique:users',
