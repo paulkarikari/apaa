d5dfebccceeaaba955fc447b4ad7d73c3c732158
paul karikari <paulkarikari1@gmail.com>
2018-03-05 14:20:18 +0000

    update user validation

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,7 +49,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => 'required|string|max:255',
             'company' => 'nullable|string|max:255',
-            'phone' => 'required|string|min:10|max:15',
+            'phone' => 'required|string|min:10|max:15|unique:users',
             'user_type' => 'required|numeric',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
