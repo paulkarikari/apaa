7d230f3c68e24aa129af5ce00968f727bd2cc36e
paul karikari <paulkarikari1@gmail.com>
2018-03-05 14:53:51 +0000

    update seed

--- database/seeds/ServiceTableSeeder.php
+++ database/seeds/ServiceTableSeeder.php
@@ -12,12 +12,12 @@ class ServiceTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(User::class)->create([
+        /* factory(User::class)->create([
             'email' => 'paulkarikari1@gmail.com',
             'user_type' => 2,
-        ]);
+        ]); */
 
-        factory(User::class)->create([
+        /* factory(User::class)->create([
             'email' => 'client@gmail.com',
             'user_type' => 1,
         ]);
@@ -27,7 +27,7 @@ class ServiceTableSeeder extends Seeder
 
         factory(User::class, 2)->create([
             'user_type' => 1,
-        ]);
+        ]); */
 
         factory(Category::class)->create([
             'category_name' => 'Construction',
@@ -48,30 +48,30 @@ class ServiceTableSeeder extends Seeder
         factory(Category::class)->create([
             'category_name' => 'IT',
         ]);
-
-        factory(Service::class, 10)->create([
-            'category_id' => 1,
-            'user_id' => 1,
-        ]);
-        factory(Service::class, 10)->create([
-            'category_id' => 2,
-            'user_id' => 1,
-        ]);
-        factory(Service::class, 10)->create([
-            'category_id' => 3,
-            'user_id' => 1,
-        ]);
-        factory(Service::class, 10)->create([
-            'category_id' => 4,
-            'user_id' => 1,
-        ]);
-        factory(Service::class, 10)->create([
-            'category_id' => 5,
-            'user_id' => 1,
-        ]);
-        factory(Service::class, 10)->create([
-            'category_id' => 6,
-            'user_id' => 1,
-        ]);
+        /*
+                factory(Service::class, 10)->create([
+                    'category_id' => 1,
+                    'user_id' => 1,
+                ]);
+                factory(Service::class, 10)->create([
+                    'category_id' => 2,
+                    'user_id' => 1,
+                ]);
+                factory(Service::class, 10)->create([
+                    'category_id' => 3,
+                    'user_id' => 1,
+                ]);
+                factory(Service::class, 10)->create([
+                    'category_id' => 4,
+                    'user_id' => 1,
+                ]);
+                factory(Service::class, 10)->create([
+                    'category_id' => 5,
+                    'user_id' => 1,
+                ]);
+                factory(Service::class, 10)->create([
+                    'category_id' => 6,
+                    'user_id' => 1,
+                ]); */
     }
 }
